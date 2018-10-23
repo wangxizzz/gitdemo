4a2cbd0c966f912c052a7b04b92da2e38fa919de
wangxizzz <979675820@qq.com>
2018-10-23 18:21:55 +0800

    first

---
+++ src/main/java/com/it/gitdemo/GitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.it.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+}
