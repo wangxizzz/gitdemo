f55c3c0d5ba19b7c7433e40a85db6bf3769952d3
wangxizzz <979675820@qq.com>
2019-09-01 20:11:30 +0800

    Revert "再一次模拟回滚"
    This reverts commit fee600ce33ba96a1a2ac2b02aec824854007fc73.

---
+++ src/main/java/com/it/gitdemo/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.it.gitdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    public void get(){
+
+    }
+}
